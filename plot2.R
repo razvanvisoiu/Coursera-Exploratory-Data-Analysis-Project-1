a39cda829be49b9c69e0342c28a3a9c0e5ecf069
razvanvisoiu <48695917+razvanvisoiu@users.noreply.github.com>
2020-08-31 01:37:14 +0300

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,7 @@
+data <- read.table("household_power_consumption.txt", header = T,sep = ";", na.strings = "?",dec='.')
+data_February_2007<-data[data$Date %in% c('1/2/2007','2/2/2007'),]
+
+date_time<-strptime(paste(data_February_2007$Date,data_February_2007$Time,sep=' ') ,'%d/%m/%Y %H:%M:%S')
+png('plot2.png',width=480,height=480)
+plot(date_time,data_February_2007$Global_active_power,xlab='',ylab='Global Active Power(kilowatts)',type='l')
+dev.off()
